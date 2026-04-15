cfd22d79ed1245a9a957b3b8465197abfdeacc13
Faraaz Mohsin <49964697+faraazmohsin@users.noreply.github.com>
2026-04-14 23:17:01 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
+import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
+const image = ref(socksBlueImage)
+
+const url = ref('https://via.placeholder.com/150')
 
 </script>
 
@@ -9,7 +14,10 @@ const product = ref('Socks')
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <img :src="image">
+      </div>
+      <div class="product-url">
+        <a :href="url">View Product</a>
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
